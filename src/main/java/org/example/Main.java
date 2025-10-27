aec94c00e34351dff3599d0d2b7715d1d5c265aa
Sinanf <sinanfozdemir@gmail.com>
2025-10-27 16:13:09 +0300

    Java Conditions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,39 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if ( clock < 0 || clock > 23) {
+            return false;
+        }
+        if (!isBarking) {
+            return false;
+        }
+        return clock < 8 || clock > 22;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       return isTeen(firstAge) || isTeen(secondAge) || isTeen(thirdAge);
+    }
+
+    public static boolean isTeen(int age) {
+       return age >= 13 && age <= 19;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+     int upper = isSummer ? 45 : 35;
+     return temp >= 25 && temp <= upper;
     }
 
     public static double area(double width, double height) {
-       
+       if( width < 0 || height < 0) {
+           return -1;
+       }
+       return width * height;
     }
 
     public static double area(double radius) {
-       
+       if (radius < 0) {
+           return -1;
+       }
+       return Math.PI * radius * radius;
     }
 }
